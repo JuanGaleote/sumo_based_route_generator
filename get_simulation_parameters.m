83759595677432ec0efc3dc2625890684da632d3
Juan Elías Galeote Cazorla <91273047+JuanGaleote@users.noreply.github.com>
2021-10-31 23:36:00 +0100

    Update get_simulation_parameters.m

--- get_simulation_parameters.m
+++ get_simulation_parameters.m
@@ -1,15 +1,6 @@
-function [Nped,Npas,Ts] = get_simulation_parameters(root)
+function [Nped,Npas,Nbike,Ts] = get_simulation_parameters(root)
 
-% Counting pedestrians trips.
-
-fid = fopen([root,'.pedestrian.trips.xml'],'r','n','UTF-8');
-text = fread(fid, [1, Inf], '*char');
-fclose(fid);
-
-k = strfind(text,'<person');
-Nped = str2double(extractBetween(text(k(end):end),'id="ped','"')) + 1;
-
-% Counting passeger trips.
+% Counting passeger trips (necessary).
 
 fid = fopen([root,'.passenger.trips.xml'],'r','n','UTF-8');
 text = fread(fid, [1, Inf], '*char');
@@ -24,6 +15,32 @@ k1 = strfind(text,'<time>');
 k2 = strfind(text,'</time');
 T = str2double(extractBetween(text(k1:k2),'"','"'));
 Ts = T(end) - T(1);
+
+% Counting pedestrians trips.
+
+fid = fopen([root,'.pedestrian.trips.xml'],'r','n','UTF-8');
+if fid ~= -1
+    text = fread(fid, [1, Inf], '*char');
+    fclose(fid);
+
+    k = strfind(text,'<person');
+    Nped = str2double(extractBetween(text(k(end):end),'id="ped','"')) + 1;
+else
+    Nped = 0;
+end
+
+% Counting bicycle trips.
+
+fid = fopen([root,'.bicycle.trips.xml'],'r','n','UTF-8');
+if fid ~= -1
+    text = fread(fid, [1, Inf], '*char');
+    fclose(fid);
+
+    k = strfind(text,'<trip');
+    Nbike = str2double(extractBetween(text(k(end):end),'id="bike','"')) + 1;
+else
+    Nbike = 0;
+end
 
 end
 
